1326fcc930716c772cfc9d7093647feece78c2de
Tony Ha <tonytaesung.ha@gmail.com>
2018-02-09 18:47:05 -0500

    Added category screen. Added create_note_element function to build note elements. Created loadNotes and renderNotes function to call note data from server and pass it into the create_note_element function.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
   const show_home_screen = function(){
-    alert("why?");
     $('.page-add.vertical-center').show();
   };
 
   const hide_home_screen = function(){
     $('.page-add.vertical-center').hide();
+  };
+
+  //creat note element
+  const create_note_element = function(data){
+    let noteElement = $('<form>').text(data.text).addClass('item');
+    let icon = $('<i>').addClass('material-icons').text('check_circle');
+    noteElement.prepend(icon);
+    return noteElement;
   };
 
   $('.page-add.vertical-center a').on('click', function(){
@@ -19,6 +26,40 @@ $(document).ready(function(){
   $('.navbar a.navbar-brand').on('click', function(){
     show_home_screen();
   });
+
+
+  const loadNotes = function(callback){
+    $.get('/notes/eat', function(data){
+      console.log("data: ", data);
+      callback(data);
+    });
+  };
+
+  //calling data from server
+  const renderNotes = function(data){
+    $('.saved-item').empty();
+    data.forEach(function(noteData, index){
+      let note = create_note_element(noteData);
+      $('.saved-item').prepend(note);
+    });
+  };
+
+  loadNotes(renderNotes);
+
+  //   const renderTweets = function(data){
+  //   $('.tweets-container').empty();
+  //   data.forEach(function(user, index){
+  //     let tweet = createTweetElement(user);
+  //     $('.tweets-container').prepend(tweet);
+  //   });
+  // };
+
+  // //calls tweet data from server
+  // const loadTweets = function(callback){
+  //   $.get('/tweets', function(data){
+  //     callback(data);
+  //   });
+  // };
 
 });
 
