5b3caa0efdad384e73d3fad972dc85221959809a
Tony Ha <tonytaesung.ha@gmail.com>
2018-02-09 15:05:01 -0500

    Refactored server.js code, moving the functions into /lib/dataHelpers.js and moving the routes into /routes/users.js. Tested for functionality.

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,61 @@
 
 const express = require('express');
 const router  = express.Router();
+const path = require('path');
 
-module.exports = (knex) => {
-
-  router.get("/", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .then((results) => {
-        res.json(results);
-    });
+module.exports = (dataHelpers) => {
+    //get app page
+  router.get('/app', (req, res) => {
+    if (req.session.user_id){
+      res.sendFile(path.resolve('./public/app.html'));
+    } else {
+      //redirect to login screen if not logged in
+      res.redirect('/login');
+    }
   });
 
+  //routing to test form
+  router.get("/testForm", (req, res) => {
+    res.sendFile(path.resolve('./public/testForm.html'));
+  });
+
+  //logs user to table 'users'
+  router.post("/register", (req, res) => {
+    dataHelpers.registerUser(req, res);
+  });
+
+  //register page
+  router.get("/register", (req, res) => {
+    res.sendFile(path.resolve('./public/register.html'));
+  });
+
+  //log user in
+  router.post("/login", (req, res) => {
+    dataHelpers.loginUser(req, res);
+  });
+
+  router.get("/login", (req, res) => {
+    res.sendFile(path.resolve('./public/login.html'));
+  });
+
+  //log user out
+  router.post("/logout", (req, res) => {
+    dataHelpers.logoutUser(req, res);
+  });
+
+  //logs note into table 'notes'
+  router.post("/note", (req, res) => {
+    dataHelpers.postNote(req, res);
+  });
+
+  //reads notes from category and user specified
+  router.get("/notes/:category", (req, res) => {
+    dataHelpers.readNote(req, res);
+  });
+
+  // Home page
+  router.get("/", (req, res) => {
+    res.sendFile(path.resolve('./public/login.html'));
+  });
   return router;
 }
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 "use strict";
-
 require("dotenv").config();
 
 const PORT        = process.env.PORT || 8080;
@@ -9,7 +8,6 @@ const bodyParser  = require("body-parser");
 const sass        = require("node-sass-middleware");
 const app         = express();
 
-const sort        = require("./scripts/sorter");
 const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require("morgan");
@@ -22,8 +20,15 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000
 }));
 
-// Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
+// Separate routes file
+//import functions
+const dataHelpers = require("./lib/dataHelpers.js")(knex);
+//import routes with functions
+const usersRoutes = require("./routes/users.js")(dataHelpers);
+
+// const dataHelpers = require("./lib/data-helpers.js")(tweetsDatabase);
+// const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -43,152 +48,8 @@ app.use("/styles", sass({
 }));
 app.use(express.static("public"));
 
-// Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
-
-// ===================================================================================================================
-// FUNCTIONS
-// ===================================================================================================================
-
-//adds user entry to users table
-const registerUser = function(req, res){
-  let email = req.body.email;
-  let password = req.body.password;
-  let operation = knex("users").insert({"email": email, "password": password});
-  operation.then((rows) => {
-    let user_id = knex().select('user_id').from('users').where('email', email);
-    user_id.then((rows) => {
-      console.log("rows: ", rows[0]);
-      req.session.user_id = rows[0].user_id;
-      res.redirect("/app");
-    });
-  });
-};
-
-//add note entry to notes table
-const postNote = function(req, res){
-  if (req.session.user_id){
-    //if logged in
-    let text = req.body.text;
-    let category = sort.entry(text);
-    let user_id = req.session.user_id;
-    let date = new Date();
-    let isoDate = date.toISOString();
-    let created_at = isoDate;
-    let operation = knex("notes").insert({"user_id": user_id, "category": category, "text": text, "created_at": isoDate});
-    operation.then((rows) => {
-      res.redirect("/app");
-    }).catch((error) => {
-      console.error("error: ", error);
-    });
-  } else {
-    //if not logged in
-    res.redirect('/login');
-  }
-};
-
-//reads notes from notes table, queried by user id and category
-const readNote = function(req, res){
-  if (req.session.user_id){
-    //if logged in
-    let user_id = req.session.user_id;
-    let category = req.params.category;
-    let operation = knex().select("text").from("notes").where("user_id", user_id).andWhere("category", category);
-    operation.then((rows) => {
-      console.log("notes read: ", rows);
-      res.send(rows);
-    }).catch((error) => {
-      console.error("error: ", error);
-    });
-  } else {
-    //if not logged in
-    res.redirect('/login');
-  }
-};
-
-//logs user in by searching the user database for matching credentials. If the matching credentials are found,
-//the user is given a session
-const loginUser = function(req, res){
-  let operation = knex().select("email", "user_id").from("users").where("email", req.body.email).andWhere("password", req.body.password);
-  operation.then((rows) => {
-    if(rows.length > 0){
-      //if the login credentials are correct, log the user in.
-      req.session.user_id = rows[0].user_id;
-      res.redirect('/app')
-    }else{
-      //if the login credentials are invalid
-      console.log("Account not found");
-      res.status(404).send();
-    }
-  }).catch((error) => {
-    console.error("error: ", error);
-  });
-};
-
-//logs the user out by ending the user's session
-const logoutUser = function(req, res){
-  //log user out
-  req.session.user_id = null;
-  res.redirect('/login');
-};
-
-// ===================================================================================================================
-// ROUTING
-// ===================================================================================================================
-
-//get app page
-app.get('/app', (req, res) => {
-  if (req.session.user_id){
-    res.sendFile('public/app.html', {root: __dirname});
-  } else {
-    //redirect to login screen if not logged in
-    res.redirect('/login');
-  }
-});
-
-//routing to test form
-app.get("/testForm", (req, res) => {
-  res.sendFile("public/testForm.html", {root: __dirname});
-});
-
-//logs user to table 'users'
-app.post("/register", (req, res) => {
-  registerUser(req, res);
-});
-
-//register page
-app.get("/register", (req, res) => {
-  res.sendFile('public/register.html', {root: __dirname});
-});
-
-//log user in
-app.post("/login", (req, res) => {
-  loginUser(req, res);
-});
-
-app.get("/login", (req, res) => {
-  res.sendFile("public/login.html", {root: __dirname});
-});
-
-//log user out
-app.post("/logout", (req, res) => {
-  logoutUser(req, res);
-});
-
-//logs note into table 'notes'
-app.post("/note", (req, res) => {
-  postNote(req, res);
-});
-
-//reads notes from category and user specified
-app.get("/notes/:category", (req, res) => {
-  readNote(req, res);
-});
-
-// Home page
-app.get("/", (req, res) => {
-  res.sendFile("public/login.html", {root: __dirname});
-});
+//send requests to router
+app.use('/', usersRoutes);
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
