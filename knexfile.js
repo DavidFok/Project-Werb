6c61481ee09e623184a635cde2cdb3488615b6d5
Tony Ha <tonytaesung.ha@gmail.com>
2018-02-19 12:16:11 -0800

    Heroku configuration attempt 2

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,8 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    // connection: process.env.DATABASE_URL + '?ssl=true',
+    connection: "postgres://ivrgvnogmzwtcv:7bc5818b89e60b50d753083f6897eafd5cf7abc674e67c59d8b978be42b32544@ec2-184-72-219-186.compute-1.amazonaws.com:5432/daglk1i7be7t3p"
     pool: {
       min: 2,
       max: 10
