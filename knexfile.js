480df6a24fd0080aa2433b8b1b1dfa1d0c1a8f05
Tony Ha <tonytaesung.ha@gmail.com>
2018-02-19 12:59:31 -0800

    hca 9

--- knexfile.js
+++ knexfile.js
@@ -24,16 +24,16 @@ module.exports = {
   production: {
     client: 'postgresql',
     // connection: process.env.DATABASE_URL + '?ssl=true',
-    connection: {
-      //host     : process.env.DB_HOST,
-      host: "postgres://ivrgvnogmzwtcv:7bc5818b89e60b50d753083f6897eafd5cf7abc674e67c59d8b978be42b32544@ec2-184-72-219-186.compute-1.amazonaws.com:5432/daglk1i7be7t3p" + '?ssl=true',
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
-    },
-
+    // connection: {
+    //   //host     : process.env.DB_HOST,
+    //   host: "postgres://ivrgvnogmzwtcv:7bc5818b89e60b50d753083f6897eafd5cf7abc674e67c59d8b978be42b32544@ec2-184-72-219-186.compute-1.amazonaws.com:5432/daglk1i7be7t3p" + '?ssl=true',
+    //   user     : process.env.DB_USER,
+    //   password : process.env.DB_PASS,
+    //   database : process.env.DB_NAME,
+    //   port     : process.env.DB_PORT,
+    //   ssl      : process.env.DB_SSL
+    // },
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
